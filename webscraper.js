5d93beaa162e9f62280c8970252b3625e5029328
PeterG24 <44652780+PeterG24@users.noreply.github.com>
2019-06-23 12:17:02 +0100

    Add files via upload

--- webscraper.js
+++ webscraper.js
@@ -1,36 +1,48 @@
-const request = require('request promise');
+const rp = require('request-promise');
 const cheerio = require('cheerio');
-//Parses through text for dates, times, numbers, emails, places
-var kwnl = require('./knwl.js')
-var knwlInstance = new knwl;
+
+var Knwl = require('knwl.js');
+var knwlInstance = new Knwl('english');
+
+//The information we will be gathering using Knwl
+var emails = [];
+var phones = [];
+var places = [];
+var links = [];
 
 //Email address used for web scraping
 var emailAddress = 'tim@canddi.com'
 
 //Remove the local part of the email address, leaving the domain
-var emailLink = emailAddress.substring(email.lastIndexOf("@") + 1);
+var emailLink = emailAddress.substring(emailAddress.lastIndexOf("@") + 1);
 
-//Using request, access the website through the domain
-request('emailLink', (error, response, html) => 
-{
-	if(!error && response.statusCode == 200)
+//Complete the url to be searched
+var url = ('https://www.' + emailLink);
+
+rp(url)
+	.then(function(html)
 	{
-		const $ = cheerio.load(html);
-	}
-	else
+		//Url is successful, begin to scrape for information
+		console.log("Website successfully found!");
+
+		knwlInstance.init(html);
+
+		//Retrieve the information from the html
+		emails = knwlInstance.get('emails');
+		phones = knwlInstance.get('phones');
+		places = knwlInstance.get('places');
+		links = knwlInstance.get('links');
+
+		//Log the information gathered
+		console.log(emails);
+		console.log(phones);
+		console.log(places);
+		console.log(links);
+	})
+	.catch(function(err)
+	{
+		//Unable to location the website, display in error in console + url
 		console.log("Unable to locate website");
-});
-
-//Using knwl, gather all the written data from the website for the function
-knwlInstance.init("$")
-
-//Function will seek out specified information (emails, numbers etc) from the website
-function retrieveData() 
-{
-	knwlInstance.get('emails');
-	knwlInstance.get('numbers');
-	knwlInstance.get('places');
-}
-
-//Display the specified data in the console
-console.log(retrieveData());
+		console.log(url);
+		console.log(err);
+	});
